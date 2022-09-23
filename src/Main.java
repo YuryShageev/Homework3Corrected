445318b938211d5c11b5c7733737ac65293e8cc5
YuryShageev <schageev.yur@yandex.ru>
2022-09-23 17:16:29 +0300

    Делаю по разбору

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
         System.out.println("Task 1");
         int applesCount = 5;
         System.out.println("Значение переменной applesCount с типом int равно " + applesCount);
-
+        byte orangeCount = 4;
+        System.out.println("Значение переменной applesCount с типом byte равно " + orangeCount);
+        short catCount = 3;
+        System.out.println("Значение переменной applesCount с типом short равно " + catCount);
+        long peopleCount = 7_500_000_000L;
+        System.out.println("Значение переменной applesCount с типом long равно " + peopleCount);
+        float pi = 3.141592f;
+        System.out.println("Значение переменной applesCount с типом float равно " + pi);
+        double e = 2.8; //основание натурального логарифма
+        System.out.println("Значение переменной applesCount с типом double равно " + e);
     }
 }
